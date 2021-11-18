677cbd5ea9a031a71943d90a145d4aefa197a681
Cristóbal <cristobal_jjg@hotmail.com>
2021-11-18 18:26:30 +0000

    IS2-Kata5; P1 V3

--- src/kata5/main/DB/DBQuerys.java
+++ src/kata5/main/DB/DBQuerys.java
@@ -1,10 +1,11 @@
 package kata5.main.DB;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class DBQuerys {
     
@@ -25,14 +26,31 @@ public class DBQuerys {
     }
     
     public void createTable(){        
-        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
-                    + " Id integer PRIMARY KEY AUTOINCREMENT,\n"
-                    + " Mail text NOT NULL);";
+        String sql = """
+                     CREATE TABLE IF NOT EXISTS EMAIL (
+                      Id integer PRIMARY KEY AUTOINCREMENT,
+                      Mail text NOT NULL);
+                     """;
         try (Connection conn = DBConnection.connect();
             Statement stmt = conn.createStatement()) {
-            // Se crea la nueva tabla
             stmt.execute(sql);
             System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void insert(List<String> emails) {
+        String sql = "INSERT INTO EMAIL(Mail) VALUES(?)";
+        try (Connection conn = DBConnection.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            
+            for (String email : emails) {
+                pstmt.setString(1, email);
+                pstmt.executeUpdate();
+                System.out.println(email + " ha sido añadido...");
+            }
+            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- src/kata5/main/Kata5.java
+++ src/kata5/main/Kata5.java
@@ -1,11 +1,13 @@
 package kata5.main;
 
+import java.util.List;
 import kata5.main.DB.DBQuerys;
 
 public class Kata5 {
     public static void main(String[] args){
         //execute_1();
-        execute_2();
+        //execute_2();
+        execute_3();
     }
     
     private static void execute_1(){
@@ -17,5 +19,12 @@ public class Kata5 {
         DBQuerys dbq = new DBQuerys();
         dbq.createTable();
     }
-
+    
+    private static void execute_3(){
+        DBQuerys dbq = new DBQuerys();
+        MailListReader rd = new MailListReader("email.txt");
+        List<String> mails = rd.getList();
+           
+        dbq.insert(mails);
+    }
 }
